a6e19ee7283c5b59a0125b70209d15c84cc84147
Sohamcalfus <soham.kasurde@calfus.com>
2025-07-07 00:42:31 -0700

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FBDIUploader from "./FBDIUploader";
+ 
+function App() {
+  return (
+<div className="App">
+<h1>FBDI Generator Tool</h1>
+<FBDIUploader />
+</div>
+  );
+}
+ 
+export default App;
+
